5710f7b1f6e09276f2a3b75ec8c31196f441326e
kojokwakye <kojokwakye808@gmail.com>
2023-06-13 15:56:14 +0000

    first step

--- external.js
+++ external.js
@@ -1,9 +1,10 @@
 // 16x16 grid of square divs
+
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
-    let square = document.createElement("div");
-    square.className = "square";
-    document.body.appendChild(square);
+    let squares = document.createElement("div");
+    squares.className = "squares";
+    document.body.appendChild(squares);
   }
   const container = document.querySelector("#container");
   document.body.appendChild(container);
