41dd7a2d318d1225b853f64a83ced30340a2d4ad
kojokwakye <kojokwakye808@gmail.com>
2023-06-15 14:55:36 +0000

    kinda perfect width

--- external.js
+++ external.js
@@ -1,12 +1,9 @@
 const container = document.getElementById("container");
-// const squares = document.createElement("div");
-// squares.classList.add("squares");
-// squares.textContent = "squares here";
 
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
     const squares = document.createElement("div");
-    squares.classList.add("squares"); // Add CSS class to style the grid cell
+    squares.classList.add("squares");
     container.appendChild(squares);
   }
 }
