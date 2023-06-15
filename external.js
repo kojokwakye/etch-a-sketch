1bc4748ca067e356bd9242a91d3a5eb23a4e3da9
kojokwakye <kojokwakye808@gmail.com>
2023-06-15 14:48:44 +0000

    squares created

--- external.js
+++ external.js
@@ -1,5 +1,12 @@
 const container = document.getElementById("container");
-const squares = document.createElement("div");
-squares.classList.add("sqaures");
+// const squares = document.createElement("div");
+// squares.classList.add("squares");
+// squares.textContent = "squares here";
 
-container.appendChild(squares);
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    const squares = document.createElement("div");
+    squares.classList.add("squares"); // Add CSS class to style the grid cell
+    container.appendChild(squares);
+  }
+}
