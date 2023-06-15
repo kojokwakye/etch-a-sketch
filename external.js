213cd70cc3dc343dca129835fd543103a24986fe
kojokwakye <kojokwakye808@gmail.com>
2023-06-15 14:34:43 +0000

    append squares into container

--- external.js
+++ external.js
@@ -1,11 +1,5 @@
-// 16x16 grid of square divs
+const container = document.getElementById("container");
+const squares = document.createElement("div");
+squares.classList.add("sqaures");
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    let squares = document.createElement("div");
-    squares.className = "squares";
-    document.body.appendChild(squares);
-  }
-  const container = document.querySelector("#container");
-  document.body.appendChild(container);
-}
+container.appendChild(squares);
