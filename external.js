e196c4fb825e858acfa3acc5ba7573e973876f2d
kojokwakye <kojokwakye808@gmail.com>
2023-06-23 17:30:52 +0000

    add isDrawing variable

--- external.js
+++ external.js
@@ -7,6 +7,10 @@ for (let i = 0; i < 16; i++) {
     container.appendChild(squares);
   }
 }
+
+// variable to track if drawing is allowed or not
+let isDrawing = false;
+
 // buttons
 const setGrid = document.getElementById("set-grid");
 const drawButton = document.getElementById("draw");
@@ -18,34 +22,60 @@ const shadeBoxes = document.querySelectorAll(".squares");
 function draw() {
   shadeBoxes.forEach((squares) => {
     squares.addEventListener("mouseenter", () => {
-      squares.style.backgroundColor = "black";
+      // check if drawing is allowed
+      if (isDrawing) {
+        squares.style.backgroundColor = "black";
+      }
     });
   });
 }
-drawButton.addEventListener("click", draw);
+
+drawButton.addEventListener("click", () => {
+  isDrawing = true;
+  draw();
+});
+
+// stop drawing when clicking on the grid
+container.addEventListener("click", () => {
+  isDrawing = false;
+});
 
 // erase boxes
 const eraseBoxes = document.querySelectorAll(".squares");
 function eraser() {
   eraseBoxes.forEach((squares) => {
     squares.addEventListener("mouseenter", () => {
-      squares.style.backgroundColor = "";
+      // check erasing
+      if (isDrawing) {
+        squares.style.backgroundColor = "";
+      }
     });
   });
 }
-eraseButton.addEventListener("click", eraser);
+// stop erasing when clicking on the grid
+eraseButton.addEventListener("click", () => {
+  isDrawing = true;
+  eraser();
+});
 
 // rainbow-shade
 const rainbowShade = document.querySelectorAll(".squares");
 function rgbShade() {
   rainbowShade.forEach((squares) => {
     squares.addEventListener("mouseenter", () => {
-      const red = Math.floor(Math.random() * 256);
-      const green = Math.floor(Math.random() * 256);
-      const blue = Math.floor(Math.random() * 256);
-      const rgbColor = `rgb(${red}, ${green}, ${blue})`;
-      squares.style.backgroundColor = rgbColor;
+      // check drawing
+      if (isDrawing) {
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        const rgbColor = `rgb(${red}, ${green}, ${blue})`;
+        squares.style.backgroundColor = rgbColor;
+      }
     });
   });
 }
-rgbButton.addEventListener("click", rgbShade);
+// stop drawing when clicking on the grid
+rgbButton.addEventListener("click", () => {
+  isDrawing = true;
+  rgbShade();
+});
