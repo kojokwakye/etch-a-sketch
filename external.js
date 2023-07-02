4e795243dd96c80f70b20d71cd95eb0debb5465a
kojokwakye <kojokwakye808@gmail.com>
2023-07-02 14:15:11 +0000

    major update

--- external.js
+++ external.js
@@ -112,6 +112,7 @@ function clearGrid() {
 }
 resetButton.addEventListener("click", clearGrid);
 
+// screenshot
 $("#screenshot").on("click", function () {
   html2canvas(document.querySelector("#container")).then((canvas) => {
     canvas.toBlob(function (blob) {
